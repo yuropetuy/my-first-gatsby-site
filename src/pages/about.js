1c654a319b230e34d908e2bbf435466817367afe
Brent Son <brentson00@gmail.com>
2023-12-18 19:31:21 -0800

    first commit creating index page and about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as  React from "react";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>Hello! I am the maker of this site using Gatsby</p>
+    </main>
+  )
+}  
+export const Head = () => <title>About Me</title>
+
+export default AboutPage
